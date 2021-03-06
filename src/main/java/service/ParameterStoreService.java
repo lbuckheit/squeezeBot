5e9a0192c89dc7401cf690acd7b723429989ef60
Luke Buckheit <lukebuckheit@gmail.com>
2021-03-06 01:09:34 -0500

    updated comment

--- src/main/java/service/ParameterStoreService.java
+++ src/main/java/service/ParameterStoreService.java
@@ -1,3 +1,4 @@
+// https://github.com/awsdocs/aws-doc-sdk-examples/blob/82cb31b4cd4a0e5971d1e68cb775377ecfa0bb2f/javav2/example_code/ssm/src/main/java/com/example/ssm/GetParameter.java
 package service;
 
 import software.amazon.awssdk.regions.Region;
