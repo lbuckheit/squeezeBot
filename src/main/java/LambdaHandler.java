5fe5d61c885ff6a1d6f57c8c9609aaf0de248dae
Luke Buckheit <lukebuckheit@gmail.com>
2021-03-06 00:59:12 -0500

    finally figured out the aws maven dependencies

--- src/main/java/LambdaHandler.java
+++ src/main/java/LambdaHandler.java
@@ -1,5 +1,27 @@
+  
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+
+import service.DynamoService;
+import service.SlackService;
+import service.TwitterService;
 public class LambdaHandler {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private TwitterService twitterService;
+    private SlackService slackService;
+    private static final String BOT_NAME = System.getenv("BOT_NAME");
+    private static final String SECRET_NAME = System.getenv("SECRET_NAME");
+    private static final String REGION = System.getenv("REGION");
+    private static final String SEARCH_STRING = System.getenv("SEARCH_STRING");
+
+    public Void handleRequest(ScheduledEvent event, Context context) {
+        Se
     }
 }
---
+++ src/main/java/service/ParameterStoreService.java
@@ -0,0 +1,11 @@
+package service;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
+import software.amazon.awssdk.services.ssm.model.SsmException;
+
+public class ParameterStoreService {
+  
+}
